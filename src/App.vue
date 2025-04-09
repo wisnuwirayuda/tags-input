cac88847a3c26513ac68d505e782dbf4c1f8e9f4
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2025-04-09 23:33:47 +0700

    Membuat Komponen Pertama (Lokal & Global)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script>
+// import TagsInput from './components/TagsInput.vue';
+// export default {
+//   components: { TagsInput }
+// }
 </script>
 
 <template>
   <h1>Hello World</h1>
+  <tags-input></tags-input>
+  <tags-input />
+  <!-- <TagsInput /> -->
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import TagsInput from './components/TagsInput.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component("tags-input", TagsInput);
+app.mount('#app');
