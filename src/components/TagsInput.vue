94452cbd645aa174f055a21be60ee4ff7d117e22
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2025-04-12 13:06:51 +0700

    Penanganan Event

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -4,10 +4,23 @@
     
     <!-- <div v-if="!tags.length">You have no any tags</div> -->
 
-    <!-- <div v-for="tag in tags">{{ tag }}</div> -->
+    <div v-for="tag in tags">{{ tag }}</div>
     
     <!-- <input type="text" v-bind:value="newTag" /> -->
-    <input type="text" :value="newTag" />
+    <!-- <input 
+        type="text" 
+        :value="newTag" 
+        v-on:keydown.enter="tags.push($event.target.value)" 
+        v-on:keydown.tab.prevent="tags.push($event.target.value)" 
+    /> -->
+    <input 
+        type="text" 
+        :value="newTag" 
+        @keydown.enter="tags.push($event.target.value)" 
+        @keydown.tab.prevent="tags.push($event.target.value)" 
+    />
+
+    <!-- <button v-on:click="tags.push(newTag)">OK</button> -->
 </template>
 
 <script>
