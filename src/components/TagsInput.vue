3ac74bdb5a4442036b06151958073309a47b17aa
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2025-04-15 06:40:00 +0700

    Memberikan Style Pada Komponen

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,51 +1,54 @@
 <template>
-    <!-- <div>{{ tags }}</div> -->
-    <!-- <div v-text="tags"></div> -->
-    
-    <!-- <div v-if="!tags.length">You have no any tags</div> -->
+    <div class="tags-input-wrapper">
+        <!-- <div>{{ tags }}</div> -->
+        <!-- <div v-text="tags"></div> -->
+        
+        <!-- <div v-if="!tags.length">You have no any tags</div> -->
 
-    <div v-for="(tag, index) in tags">
-        {{ tag }}
-        <a href="#" @click.prevent="removeTag(index)">&times;</a>
+        <span class="tag-item" v-for="(tag, index) in tags" :key="index">
+            {{ tag }}
+            <a class="remove-tag" href="#" @click.prevent="removeTag(index)">&times;</a>
+        </span>
+        
+        <!-- <input type="text" v-bind:value="newTag" /> -->
+        <!-- <input 
+            type="text" 
+            :value="newTag" 
+            v-on:keydown.enter="tags.push($event.target.value)" 
+            v-on:keydown.tab.prevent="tags.push($event.target.value)" 
+        /> -->
+
+        <!-- <hr>
+        <pre>{{ newTag }}</pre> -->
+        <!-- <input 
+            type="text" 
+            :value="newTag" 
+            @input="newTag = $event.target.value"
+            @keydown.enter="tags.push(newTag)" 
+            @keydown.tab.prevent="tags.push(newTag)" 
+        /> -->
+
+        <!-- <input 
+            type="text" 
+            v-model.trim="newTag"
+            @keydown.enter="addNewTag" 
+            @keydown.tab.prevent="addNewTag"
+            @keydown.delete="removeLastTag"
+            :class="tags.includes(newTag) ? 'tag-exists' : ''"
+        /> -->
+
+        <input 
+            class="tag-input"
+            type="text" 
+            v-model.trim="newTag"
+            @keydown.enter="addNewTag" 
+            @keydown.tab.prevent="addNewTag"
+            @keydown.delete="removeLastTag"
+            :class="{ 'tag-exists': isTagExists }"
+        />
+        
+        <!-- <button v-on:click="tags.push(newTag)">OK</button> -->
     </div>
-    
-    <!-- <input type="text" v-bind:value="newTag" /> -->
-    <!-- <input 
-        type="text" 
-        :value="newTag" 
-        v-on:keydown.enter="tags.push($event.target.value)" 
-        v-on:keydown.tab.prevent="tags.push($event.target.value)" 
-    /> -->
-
-    <hr>
-    <pre>{{ newTag }}</pre>
-    <!-- <input 
-        type="text" 
-        :value="newTag" 
-        @input="newTag = $event.target.value"
-        @keydown.enter="tags.push(newTag)" 
-        @keydown.tab.prevent="tags.push(newTag)" 
-    /> -->
-
-    <!-- <input 
-        type="text" 
-        v-model.trim="newTag"
-        @keydown.enter="addNewTag" 
-        @keydown.tab.prevent="addNewTag"
-        @keydown.delete="removeLastTag"
-        :class="tags.includes(newTag) ? 'tag-exists' : ''"
-    /> -->
-
-    <input 
-        type="text" 
-        v-model.trim="newTag"
-        @keydown.enter="addNewTag" 
-        @keydown.tab.prevent="addNewTag"
-        @keydown.delete="removeLastTag"
-        :class="{ 'tag-exists': isTagExists }"
-    />
-
-    <!-- <button v-on:click="tags.push(newTag)">OK</button> -->
 </template>
 
 <script>
@@ -97,8 +100,49 @@ export default {
 </script>
 
 <style scoped>
-.tag-exists {
+.tag-input.tag-exists {
     color: red;
     text-decoration: line-through;
+}
+
+.tags-input-wrapper {
+    background: #fff;
+    padding: 0.5em;
+    border: 1px solid #dbdbdb;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    min-height: 36px;
+    box-sizing: border-box;
+}
+
+.tag-item {
+    color: #212529;
+    background-color: #eee;
+    margin-right: 0.3em;
+    padding: 0.25em 0.4em;
+    font-size: 75%;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-right: 1.25em;
+    padding-left: 0.6em;
+}
+
+.tag-input {
+    color: #495057;
+    flex: 1;
+    background: transparent;
+    border: none;
+}
+
+.tag-input:focus {
+    outline: none;
+}
+
+a.remove-tag {
+    text-decoration: none;
 }
 </style>
